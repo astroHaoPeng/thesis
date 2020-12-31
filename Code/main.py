778f109b40ffaa66ebc519f70091526f4af96c0b
fedecasali <federico.casali.97@gmail.com>
2020-12-31 13:02:05 +0100

    delete unnecesary main file

--- Code/main.py
+++
@@ -1,7 +0,0 @@
-# This is a sample Python script.
-import numpy as np
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-print(np.pi)
